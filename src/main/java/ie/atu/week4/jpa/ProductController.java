59aa3daa227bcf588eea47d8cda048ece1b185a0
rossa <g00401655@atu.ie>
2024-10-08 20:02:48 +0100

    Finished HW

--- src/main/java/ie/atu/week4/jpa/ProductController.java
+++ src/main/java/ie/atu/week4/jpa/ProductController.java
@@ -9,32 +9,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-    private List<Product> productList = new ArrayList<>();
+
+    private final ProductRepo productRepo;
     private ProductService productService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, ProductRepo productRepo) {
         this.productService = productService;
+        this.productRepo = productRepo;
     }
-    @GetMapping("/getProducts")
+
     public List<Product> getProducts() {
-        return productList;
+        return productService.findAll();
     }
 
-    @PostMapping("/addProduct")
-    public ResponseEntity<List> addProduct(@RequestBody Product product) {
-        productList = productService.add(product);
-        return ResponseEntity.ok(productList);
+    public void addProduct(Product product) {
+      productService.add(product);
     }
 
-    @PutMapping("/updateProduct/{id}")
-    public ResponseEntity<List<Product>> updateProduct(@PathVariable int id, @RequestBody Product updatedProduct) {
-        productList = productService.updateById(updatedProduct.getId());
-        return ResponseEntity.ok(productList);
+    public void updateProduct(Long id, Product product) {
+        Product updateProduct = productRepo.getReferenceById(id);
+        updateProduct.setId(product.getId());
     }
 
-    @DeleteMapping("/deleteProduct/{id}")
-    public ResponseEntity<List<Product>> deleteProduct(@PathVariable int id) {
-        productList = productService.deleteById((long) id);
-        return ResponseEntity.ok(productList);
+    public void deleteProduct(Long id) {
+        productRepo.deleteById(id);
     }
 }
--- src/main/java/ie/atu/week4/jpa/ProductService.java
+++ src/main/java/ie/atu/week4/jpa/ProductService.java
@@ -1,6 +1,7 @@
 package ie.atu.week4.jpa;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,38 +9,29 @@ import java.util.List;
 
 @Service
 public class ProductService {
-    private ProductRepo productRepo;
-    private List<Product> productList = new ArrayList<>();
+    private final ProductRepo productRepo;
 
-    private Product findProductById(int id) {
-        for (Product product : productList) {
-            if (product.getProductCode() == id) {
-                return product;
-            }
-        }
-        return null;
-    }
-
+    @Autowired
     public ProductService(ProductRepo productRepo) {
         this.productRepo = productRepo;
     }
 
-    public List<Product> add(Product product){
-        productRepo.save(product);
+    public List<Product> findAll() {
         return productRepo.findAll();
+    }
+
+    public void add(Product product){
+        productRepo.save(product);
     }
     public List<Product> deleteById(Long id){
         productRepo.deleteById(id);
         return productRepo.findAll();
     }
 
-    public List<Product> updateById(Long id){
-        Product updatedProduct = findProductById(Math.toIntExact(id));
-        if (updatedProduct != null) {
-            updatedProduct.setProductName(updatedProduct.getProductName());
-            updatedProduct.setProductDescription(updatedProduct.getProductDescription());
-            updatedProduct.setProductPrice(updatedProduct.getProductPrice());
-        }
-        return productRepo.findAll();
+    public void update(Long id, Product product){
+        Product updatedProduct = productRepo.getReferenceById(id);
+        updatedProduct.setProductName(updatedProduct.getProductName());
+        updatedProduct.setProductDescription(updatedProduct.getProductDescription());
+        updatedProduct.setProductPrice(updatedProduct.getProductPrice());
     }
 }
